5ada7a3d3ca48025a4ee622b629c6a323eaedd63
alexdoshii <alex@doshii.io>
2023-11-25 21:14:30 +1100

    refining block

--- actions/lib/auth0_mgmt.py
+++ actions/lib/auth0_mgmt.py
@@ -74,5 +74,5 @@ class Auth0Mgmt(Action):
             "created_at": user['created_at'],
             "updated_at": user['updated_at'],
             "last_login": user['last_login'],
-            "user_block": block
+            "user_block": block if len(block['blocked_for']) > 0 else "unblocked"
         }
